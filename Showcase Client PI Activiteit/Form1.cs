9111c0878eb1479e90bcf31da33e58eb47899cfc
davidVeer <151018675+davidVeer@users.noreply.github.com>
2025-09-29 10:53:38 +0200

    Add project files.

---
+++ Showcase Client PI Activiteit/Form1.cs
@@ -0,0 +1,10 @@
+namespace Showcase_Client_PI_Activiteit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Showcase Client PI Activiteit/Program.cs
@@ -0,0 +1,17 @@
+namespace Showcase_Client_PI_Activiteit
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
